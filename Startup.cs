1f15d9e71bb025a7be07ccd966491d1cbe18f43a
Juan Arellano Pastor <juan@MacBook-Pro-de-Juan.local>
2020-06-10 17:32:11 -0500

    Carga de base de datos

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using bsiteTest.Data;
 
 namespace bsiteTest
 {
@@ -20,6 +22,9 @@ namespace bsiteTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<bsiteCtx>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("bside"));
+            });
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
